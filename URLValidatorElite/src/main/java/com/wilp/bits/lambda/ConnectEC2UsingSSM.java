b6b609e156797b789cb9e52d9afaac4ef00857cc
Gajendran R <70207067+Gaje1109@users.noreply.github.com>
2024-01-17 06:11:23 +0530

    Update ConnectEC2UsingSSM.java

--- URLValidatorElite/src/main/java/com/wilp/bits/lambda/ConnectEC2UsingSSM.java
+++ URLValidatorElite/src/main/java/com/wilp/bits/lambda/ConnectEC2UsingSSM.java
@@ -51,7 +51,7 @@ public class ConnectEC2UsingSSM implements RequestHandler<S3Event, String> {
 		InstanceUtility ec2Util = new InstanceUtility();
 		// AWS Credentials integrated
 		ReadWriteProps props = new ReadWriteProps();
-		input="ConnectEC2UsingSSM  Completed with Success";
+		//input="ConnectEC2UsingSSM  Completed with Success";
 		String[] keys = props.ReadPropsFile().split(",");
 		String accesskey = keys[0];
 		String secretkey = keys[1];
